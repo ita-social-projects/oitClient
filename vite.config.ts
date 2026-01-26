a1beaef34acf998c2290ea32fe72a0198a08314d
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-01-26 15:18:40 +0200

    Add front-back integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import type { ConfigEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false
+        }
+      }
+    }
+  })
+}
