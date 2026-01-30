2886d948d3029277678060fd3e4fa403a16c4ad3
witoldDark <witalikspelina@gmail.com>
2026-01-30 18:55:24 +0200

    coverage gitignore, fixed config, added sonar prop

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   test: {
+    include: ['**/*.test.tsx'],
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    environment: 'jsdom',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html', 'lcov'],
       reportsDirectory: './coverage',
-      exclude: ['node_modules/', 'src/main.ts'],
+      exclude: ['node_modules/', 'src/main.tsx'],
     }
-  },
+  }
 });
