730318240d3aefe513d157a71eb77fb6295c659f
Vitalii Spelina <94604123+witolDark@users.noreply.github.com>
2026-02-05 15:49:42 +0200

    Feat/i18n, MUI component library (#7)
    * added MUI, translation i18n
    
    * req changes

---
+++ src/i18n.ts
@@ -0,0 +1,22 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpBackend from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
+
+i18n
+  .use(HttpBackend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    defaultNS: 'common',
+    ns: ['common'],
+    interpolation: {
+      escapeValue: false,
+    },
+    backend: {
+      loadPath: '/i18n/{{lng}}/{{ns}}.json',
+    },
+  });
+
+export default i18n;
--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -1,11 +1,13 @@
+import LangButton from '@components/LangButton/LangButton';
 import { Outlet } from 'react-router-dom';
 
 import styles from './Auth.module.scss';
 
 export function AuthLayout() {
   return (
-    <div className={styles.page}>
+    <div className={`relative ${styles.page}`}>
       <Outlet />
+      <LangButton className="btn-regular absolute top-4 right-4 w-[180px]" />
     </div>
   );
 }
