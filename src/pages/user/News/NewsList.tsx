9cce33a922982246f9db521b8234c3396baf4d8a
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-02-05 18:15:44 +0100

    Added localization to news page

--- src/pages/user/News/NewsList.tsx
+++ src/pages/user/News/NewsList.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import NewsCard from '@components/news/NewsCard';
 import type { NewsItem } from '@shared/models/news';
 import styles from './News.module.scss';
+import { useTranslation } from 'react-i18next';
 
 export default function NewsListPage() {
   const [news, setNews] = useState<NewsItem[]>([]); 
   const [loading, setLoading] = useState(true);
+  const { t } = useTranslation('auth');
 
   useEffect(() => {
     fetch('http://localhost:3001/news')
@@ -21,16 +23,14 @@ export default function NewsListPage() {
       .finally(() => setLoading(false));
   }, []);
 
-  if (loading) return <p>Завантаження новин...</p>;
+  if (loading) return <p>{t('newsLoading')}</p>;
 
   return (
     <div className={styles.page}>
-      <h1>Новини</h1>
-      <p className={styles.subtitle}>
-        Дізнавайтеся першими про анонси та новини ІТ-олімпіад
-      </p>
+      <h1 className={styles.title}>{t('newsTitle')}</h1>
+      <p className={styles.subtitle}>{t('newsSubtitle')}</p>
       {news.length === 0 ? (
-        <p>Немає новин</p>
+        <p>{t('noNews')}</p>
       ) : (
         news.map(item => <NewsCard key={item.id} news={item} />)
       )}
