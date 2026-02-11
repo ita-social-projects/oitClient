3ce1d4307556b4475a2e95523527fe88d48aa7d2
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-02-11 12:10:42 +0100

    Remove some code

--- src/pages/user/News/NewsDetail.tsx
+++ src/pages/user/News/NewsDetail.tsx
@@ -9,22 +9,15 @@ import styles from './NewsDetail.module.scss';
 export default function NewsDetailPage() {
   const { id } = useParams<{ id: string }>();
   const [news, setNews] = useState<NewsItem | null>(null);
-  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const { t } = useTranslation('auth');
 
   useEffect(() => {
     fetch(`/news/${id}`) 
-      .then(res => {
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        return res.json();
-      })
+      .then(res =>  res.json())
       .then((data: NewsItem) => setNews(data))
-      .catch(err => console.error(err))
-      .finally(() => setLoading(false));
   }, [id]);
 
-  if (loading) return <p>{t('news.loading')}</p>;
   if (!news) return <p>{t('news.notFound')}</p>;
 
   return (
--- src/pages/user/News/NewsList.tsx
+++ src/pages/user/News/NewsList.tsx
@@ -8,26 +8,18 @@ import styles from './News.module.scss';
 
 export default function NewsListPage() {
   const [news, setNews] = useState<NewsItem[]>([]); 
-  const [loading, setLoading] = useState(true);
   const { t } = useTranslation('auth');
 
   useEffect(() => {
     fetch('/news')
-      .then(res => {
-        if (!res.ok) throw new Error(`HTTP ${res.status}`);
-        return res.json();
-      })
+      .then(res => res.json())
       .then((data: NewsItem[]) => {
         data.sort(
           (a, b) => new Date(b.publicationDate).getTime() - new Date(a.publicationDate).getTime()
         );
         setNews(data);
       })
-      .catch(err => console.error(err))
-      .finally(() => setLoading(false));
   }, []);
-
-  if (loading) return <p>{t('news.loading')}</p>;
 
   return (
     <div className={styles.page}>
