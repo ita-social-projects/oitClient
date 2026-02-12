1074789a614df8cfe2f2dbfacaf4706caf3cebb2
Vitalii Spelina <94604123+witolDark@users.noreply.github.com>
2026-02-12 17:21:39 +0200

    Revert "Feature/news page"

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,13 @@ import { AuthLayout } from './pages/auth/AuthLayout.tsx';
 import { SignIn } from './pages/auth/SignIn.tsx';
 import { SignUp } from './pages/auth/SignUp.tsx';
 import Home from './pages/public/Home.tsx';
-import NewsDetail from './pages/user/News/NewsDetail.tsx';
-import NewsList from './pages/user/News/NewsList.tsx';
 
 export default function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
-         <Route path="/news" element={<NewsList />} />
-         <Route path="/news/:id" element={<NewsDetail />} />
+        <Route path="/news" element={<div>News Page</div>} />
       </Route>
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<SignIn />} />
