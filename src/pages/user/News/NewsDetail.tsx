b2579d94703997c2f4be096ee33642992135fb84
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-02-23 15:05:43 +0100

    Removed console log

--- src/pages/user/News/NewsDetail.tsx
+++ src/pages/user/News/NewsDetail.tsx
@@ -18,10 +18,7 @@ export default function NewsDetailPage() {
   useEffect(() => {
     axios.get<NewsItem>(`/news/${id}`)
       .then(res => setNews(res.data))
-      .catch(err => {
-        console.error('Failed to fetch news:', err);
-        setNews(null); 
-      });
+      .catch(() => setNews(null));
   }, [id]);
 
   if (!news) return <p>{t('news.notFound')}</p>;
