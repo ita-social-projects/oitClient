57d0663fa57de54e218355592efa33f6ed138fa2
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-02-24 13:44:51 +0100

    Added search fields for news

--- src/pages/user/News/NewsList.tsx
+++ src/pages/user/News/NewsList.tsx
@@ -1,6 +1,6 @@
 import type { NewsItem } from '@shared/models/news';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
@@ -9,6 +9,8 @@ import NewsCard from './NewsCard';
 
 export default function NewsListPage() {
   const [news, setNews] = useState<NewsItem[]>([]);
+  const [search, setSearch] = useState<string>("");
+  const [date, setDate] = useState<string>("");
   const { t } = useTranslation('public');
 
   useEffect(() => {
@@ -20,14 +22,45 @@ export default function NewsListPage() {
       .catch(() => setNews([]));
   }, []);
 
+  const filteredNews = useMemo(() => {
+    const lowerSearch = search.toLowerCase();
+
+    return news.filter(item => {
+      const matchesText =
+        item.title.toLowerCase().includes(lowerSearch) ||
+        item.content.toLowerCase().includes(lowerSearch);
+
+      const matchesDate = !date || item.publicationDate === date;
+
+      return matchesText && matchesDate;
+    });
+
+
+  }, [news, search, date]);
+
   return (
     <div className="flex flex-col items-center py-[70px] bg-white">
       <h1 className="font-bold mb-4">{t('news.title')}</h1>
       <p className="text-sm text-meta">{t('news.subtitle')}</p>
-      {news.length === 0 ? (
+      <div className="w-full mt-8 mb-4 flex gap-4">
+        <input
+          type="text"
+          placeholder={t('news.searchPlaceholder')}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          onClick={(e) => (e.target as HTMLInputElement).showPicker?.()}
+        />
+      </div>
+
+      {filteredNews.length === 0 ? (
         <p>{t('news.noNews')}</p>
       ) : (
-        news.map(item => <NewsCard key={item.id} news={item} />)
+        filteredNews.map(item => <NewsCard key={item.id} news={item} />)
       )}
       <div className="w-full mt-16 pt-8 border-t border-gray-100">
         <p className="text-meta mb-2">
