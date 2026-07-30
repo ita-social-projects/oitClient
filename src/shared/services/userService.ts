236fa170440ad598236cb8823836733832ed125d
Murashko Vladislav <vladislavuh2000@gmail.com>
2026-07-30 15:50:13 +0300

    refactor: base_url constant removed from userService

--- src/shared/services/userService.ts
+++ src/shared/services/userService.ts
@@ -1,11 +1,9 @@
 import { axiosInstance } from '@shared/api/axiosInstance';
 import type { UserResponse, ChangeUserRoleRequest, UserDto, ChangeUserStatusRequest } from '@shared/models/user';
 
-const API_BASE = import.meta.env.VITE_API_URL;
-
 export const userService = {
   getUsers: async (page: number, size: number, search?: string) => {
-    const { data } = await axiosInstance.get<UserResponse>(`${API_BASE}/api/v1/users`, {
+    const { data } = await axiosInstance.get<UserResponse>('/api/v1/users', {
       params: {
         page,
         size,
@@ -17,13 +15,13 @@ export const userService = {
   },
 
   changeRole: async (id: number, request: ChangeUserRoleRequest) => {
-    const { data } = await axiosInstance.patch<UserDto>(`${API_BASE}/api/v1/users/${id}/role`, request);
+    const { data } = await axiosInstance.patch<UserDto>(`/api/v1/users/${id}/role`, request);
 
     return data;
   },
 
   changeStatus: async (id: number, request: ChangeUserStatusRequest) => {
-    const { data } = await axiosInstance.patch<UserDto>(`${API_BASE}/api/v1/users/${id}/status`, request);
+    const { data } = await axiosInstance.patch<UserDto>(`/api/v1/users/${id}/status`, request);
 
     return data;
   },
