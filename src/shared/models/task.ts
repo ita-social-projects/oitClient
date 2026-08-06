e3fe6ef0b00aa0dc51782a0d9dfccc1fbdfa900d
Ihor Selianyk <iseljani@gmail.com>
2026-08-06 19:53:52 +0300

    feat: create task model and all related models

---
+++ src/shared/models/task.ts
@@ -0,0 +1,29 @@
+export type TaskFileRole = 'PROBLEM' | 'REFERENCE' | 'SOLUTION';
+
+export interface TaskFileDto {
+  id: number;
+  originalFilename: string;
+  mimeType: string;
+  size: number;
+  fileRole: TaskFileRole;
+  url: string;
+}
+
+export interface TaskItem {
+  id: number;
+  title: string;
+  description: string;
+  files: TaskFileDto[];
+  createdBy: number;
+  ownerId: number;
+}
+
+export interface TaskListResponse {
+  content: TaskItem[];
+  pageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  last: boolean;
+}
