db9075d9c914bf3304eac6e0425f5b2279d72933
AnastasiaRakuta <anastasiarakuta.2001@gmail.com>
2026-08-11 11:56:31 +0200

    Adapted cabinet panel for mobile screens

--- src/layout/CabinetLayout.tsx
+++ src/layout/CabinetLayout.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { CabinetPanel } from '../pages/CabinetPanel/CabinetPanel.tsx';
 import { Header } from '../layout/Header';
 
 export const CabinetLayout = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
   return (
     <>
       <Header />
       <div className="flex">
-        <CabinetPanel />
-        <div className="flex-1 bg-white">
-          <Outlet />
+        <CabinetPanel isOpen={isDrawerOpen} onClose={() => setIsDrawerOpen(false)} />
+        <div className="flex-1 bg-white min-w-0 flex flex-col relative">
+          <div className="md:hidden flex items-center p-4 border-b border-gray-200">
+            <button onClick={() => setIsDrawerOpen(true)} className="text-gray-700 px-2">
+              <i className="fa-solid fa-ellipsis-vertical text-xl"></i>
+            </button>
+          </div>          
+          <div className="flex-1">
+            <Outlet />
+          </div>
         </div>
       </div>
     </>
